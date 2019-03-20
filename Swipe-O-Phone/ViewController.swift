19510a2ed0ebd1fdda9187216f7bb9518fece157
Jeroen Dunselman <jeroduns@gmail.com>
2019-03-20 16:32:10 +0100

    Initial Commit

---
+++ Swipe-O-Phone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swipe-O-Phone
+//
+//  Created by Jeroen Dunselman on 20/03/2019.
+//  Copyright © 2019 Jeroen Dunselman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
